93e9f2111a474e0e30decf9d06cbb3ffcbd3611d
oybek xushnazarov <oybekxushnazarov7@gmail.com>
2026-09-05 02:27:11 +0500

    Data ETL qilindi va toza data ustida tahlillar qilindi

--- Part2/5,sql.sql
+++
@@ -1,9 +0,0 @@
-SELECT empid, lastname
-FROM HR.Employees
-WHERE LEFT(lastname, 1) COLLATE Latin1_General_CI_AS LIKE '[a-z]'
-  AND LEFT(lastname, 1) COLLATE Latin1_General_CI_AS <> UPPER(LEFT(lastname, 1));
-
-
-SELECT empid, lastname
-FROM HR.Employees
-WHERE ASCII(LEFT(lastname, 1)) BETWEEN 97 AND 122;

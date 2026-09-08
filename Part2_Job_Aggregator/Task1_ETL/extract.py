a48387ac06f545d5bd894608c3d513e50732b65f
oybek xushnazarov <oybekxushnazarov7@gmail.com>
2026-09-09 01:59:20 +0500

    Task1_ETL da Data qayta ETL qilindi

--- Part2_Job_Aggregator/Task1_ETL/extract.py
+++ Part2_Job_Aggregator/Task1_ETL/extract.py
@@ -1,17 +1,15 @@
 import pandas as pd
 from pathlib import Path
 
-DATA_DIR = Path("Part2_Job_Aggregator/Task1_ETL/jobgram_org_2025")
+DATA_DIR = Path(__file__).parent / "jobgram_org_2025"
 
 def extract_data():
     raw_data = {}
-    
     for file_path in DATA_DIR.glob("*.csv"):
         name = file_path.stem
         raw_data[name] = pd.read_csv(file_path, encoding='utf-8', encoding_errors='replace')
-            
     return raw_data
 
 if __name__ == "__main__":
     data = extract_data()
-    print("O'qilgan jadvallar:", list(data.keys()))
+    print("O'qilgan fayllar:", list(data.keys()))

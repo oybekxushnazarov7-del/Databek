d59560c4373a85733b76c2d3062f3526c175a35a
oybek xushnazarov <oybekxushnazarov7@gmail.com>
2026-09-10 15:27:45 +0500

    etl.py dagi filter olib tashlandi va gold data Azure ga yuklandi

--- Part1_ETL/abituriyent_2024.sql
+++ Part1_ETL/abituriyent_2024.sql
@@ -6,15 +6,16 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'abituriyent_2024')
 BEGIN
     CREATE TABLE abituriyent_2024 (
-        ID VARCHAR(50) PRIMARY KEY,
+        row_id INT IDENTITY(1,1) PRIMARY KEY,
+        ID VARCHAR(50),
         tr_class NVARCHAR(100),
         status NVARCHAR(50),
         ball FLOAT,
         oliy_talim_muassasasi NVARCHAR(255),
-        FIO nvarchar(100),
-        yonalish nvarchar(200),
+        FIO NVARCHAR(100),
+        yonalish NVARCHAR(200),
         talim_shakli NVARCHAR(100),
-        til nvarchar(100),
+        til NVARCHAR(100)
     );
 END
 

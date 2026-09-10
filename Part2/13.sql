ed45bad7043012f94cc2f9fb83ae9505faf75225
oybek xushnazarov <oybekxushnazarov7@gmail.com>
2026-09-10 15:25:32 +0500

    22 ta query bajarildi va 4,13-querylar to'g'rilandi

--- Part2/13.sql
+++ Part2/13.sql
@@ -1,7 +1,11 @@
+GO
+USE TSQLV4
+GO
+
 select empid, firstname as Firstnmae, lastname
 from HR.Employees as E
 where not exists (
 	select O.orderdate
 	from Sales.Orders as O
-	where O.empid = E.empid and orderdate > = '2016-05-01'
+	where O.empid = E.empid and orderdate >= '2016-05-01'
 );
--- Part2/4.sql
+++ Part2/4.sql
@@ -1,5 +1,5 @@
-select orderid, sum(qty*unitprice) as [total value] 
-from Sales.OrderDetails
-where qty*unitprice >= 10000
-group by orderid
-order by [total value] desc 
+SELECT orderid, SUM(qty * unitprice) AS [total value]
+FROM Sales.OrderDetails
+GROUP BY orderid
+HAVING SUM(qty * unitprice) > 10000
+ORDER BY [total value] DESC;
